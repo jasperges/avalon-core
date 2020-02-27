9ceaff7b9d354123338aa7eaee85d0b412dee5fe
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2020-02-27 23:29:43 +0100

    Exclude cinema4d dir from testing

--- run_tests.py
+++ run_tests.py
@@ -23,6 +23,7 @@ if __name__ == "__main__":
         "--exclude-dir=avalon/maya",
         "--exclude-dir=avalon/nuke",
         "--exclude-dir=avalon/houdini",
+        "--exclude-dir=avalon/cinema4d",
 
         # We can expect any vendors to
         # be well tested beforehand.
